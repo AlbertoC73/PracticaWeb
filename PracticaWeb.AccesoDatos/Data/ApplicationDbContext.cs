c5c7ed71d8704796a2c06e235c786384289f2061
Jose Alberto <149119549+Jack6R9@users.noreply.github.com>
2024-04-25 20:39:34 -0600

    first commit

---
+++ PracticaWeb.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PracticaWeb.Modelos;
+using System.Reflection;
+
+namespace PracticaWeb.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Bodega> Bodegas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        
+        public DbSet<Marca> Marca { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ PracticaWeb.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using PracticaWeb.AccesoDatos.Data;
+using PracticaWeb.AccesoDatos.Repositorio.IRepositorio;
+using PracticaWeb.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaWeb.AccesoDatos.Repositorio
+{
+    public class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MarcaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Actualizar(Marca marca)
+        {
+            var marcaBD = _db.Marca.FirstOrDefault(b => b.Id == marca.Id);
+            if (marcaBD != null)
+            {
+                marcaBD.Nombre = marca.Nombre;
+                marcaBD.Descripcion = marca.Descripcion;
+                marcaBD.Estado = marca.Estado;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
